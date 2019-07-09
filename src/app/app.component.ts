e8f466a4c6ea2c3174f7a97bf421cbb16e673164
Kenney Curlee <bassetthnd824@gmail.com>
2019-07-08 21:46:09 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pckm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pickem';
+}
